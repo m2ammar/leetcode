8d10fab6d975c90173c25e36fa50751c975dd12a
Muhammad Ammar Saleem <ma9731501@gmail.com>
2026-07-29 15:27:49 +0500

    Update solution.sql

--- SQL/570_Managers_with_at_Least_5_Direct_Reports/solution.sql
+++ SQL/570_Managers_with_at_Least_5_Direct_Reports/solution.sql
@@ -1,1 +1,9 @@
-
+SELECT e.name
+FROM Employee AS e
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) AS report ON e.id = report.managerId;
