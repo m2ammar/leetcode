ea9424c16e1310a82ef4039a04af645f6d89a9ce
Muhammad Ammar Saleem <ma9731501@gmail.com>
2026-08-22 23:47:56 +0500

    Implement bubble sort for sorting colors

---
+++ Java/75-sort-colors/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int size = nums.length;
+        int temp = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
